54e036ca63b5568ec19bb983f401fcb4c0fba11e
Claudio Manniti <claudio.manniti@outlook.com>
2020-06-18 22:17:42 +0200

    Create Ohm's law calculator class

---
+++ GettingStartedSpecflow/OhmsLawCalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace GettingStartedSpecflow
+{
+    public class OhmsLawCalculator
+    {
+        public double CalculateVoltage(double current, double resistance)
+        {
+            return current * resistance;
+        }
+
+        public double CalculateCurrent(double voltage, double resistance)
+        {
+            return voltage / resistance;
+        }
+
+        public double CalculateResistance(double voltage, double current)
+        {
+            return voltage / current;
+        }
+    }
+}
